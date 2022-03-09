45ca8007372709ccbbe3f5659cb722edf806631b
Vincent Soweto <vincentsoweto22@gmail.com>
2022-03-09 13:43:33 +0200

    React Todo App

---
+++ src/App.js
@@ -0,0 +1,137 @@
+import Header from './components/Header'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Tasks from './components/Tasks'
+import {useState, useEffect} from 'react'
+import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
+
+const App = () =>  {
+const [showAddTask,setShowAddTask] = useState(false)
+const [tasks, setTasks] = useState([])
+
+useEffect(()=>{
+  const getTasks = async () =>{
+    const tasksFromServer = await fetchTasks() 
+    setTasks(tasksFromServer)
+  }
+  
+
+  getTasks()
+},[])
+
+// Fetch tasks
+const fetchTasks =  async () =>{
+    const res = await fetch('http://localhost:5000/tasks')
+    const data =  await res.json()
+ 
+    console.log(data)
+    return data
+
+}
+
+const fetchTask =  async (id) =>{
+    const res = await fetch('http://localhost:5000/tasks/${id}')
+    const data =  await res.json()
+ 
+    console.log(data)
+    return data
+
+}
+
+
+//Add task
+const addTask = async (task) =>{
+  const res = await fetch (`http://localhost:5000/tasks`,{
+    method: 'POST',
+    headers:{
+      'Content-type':'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data =await  res.json()
+
+  setTasks([...tasks, data])
+  // const id = Math.floor(Math.random() * 10000) + 1
+  // const newTask = {id, ...task}
+  // setTasks([...tasks,newTask])
+  // console.log(id);
+}
+
+
+//Delete task
+const deleteTask =async(id)=>{
+  const res = await fetch (`http://localhost:5000/tasks/${id}`,{
+    method: 'DELETE',
+  })
+  res.status === 200
+  ? setTasks(tasks.filter((task) => task.id !== id)) : alert('Error deleting this task')
+}
+
+//Toggle Reminder
+const toggleReminder = async (id) =>{
+  
+  const taskToToggle = await fetchTask(id)
+  const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
+  
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method:'PUT',
+    header:{
+      'Content-type':'application/json'
+    },
+    body: JSON.stringify(updTask)
+
+  })
+  const data = await res.JSON()
+
+
+  setTasks(
+    tasks.map((task) => 
+      task.id === id ? {...task, reminder: data.reminder} : task
+        )
+      )
+}
+
+
+
+  return (
+    <Router>
+    <div className='container'>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask}/>
+
+      <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  'No Tasks To Show'
+                )}
+              </>
+            }
+          />
+          <Route path='/about' element={<About />} />
+      </Routes>
+      <Footer/>
+    </div>
+
+    </Router>
+  )
+}
+
+// class App extends React.Component{
+//   render(){
+//     return <h1>Hello form a class</h1>
+//   }
+// }
+
+export default App
